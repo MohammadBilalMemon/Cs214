c06b4c727e2f23a4c37a5eb76c9154a76f0de177
MohammadBilalMemon <bilalmemon16@gmail.com>
2017-04-22 14:45:10 -0400

    Update libnetfiles.c

--- libnetfiles.c
+++ libnetfiles.c
@@ -1,70 +1,210 @@
 #include <stdio.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
-#include <sys/types.h>  //open() 
-#include <sys/stat.h>  //open()
-#include <fcntl.h> //open()
-#include <pthread.h> 
-#include <sys/socket.h>
-#include <netdb.h>
-#include "libnetfiles.h"
+int sockfd = -1;																// file descriptor for our socket
+	int portno = -1;																// server port to connect to
+	int n = -1;																		// utility variable - for monitoring reading/writing from/to the socket
+	char buffer[256];															// char array to store data going to and coming from the server
+    struct sockaddr_in serverAddressInfo;						// Super-special secret C struct that holds address info for building our socket
+    struct hostent *serverIPAddress;		
 
-int netserverinit(const char * hostname){
-	int port; // Greater than 8k and less than 64k
-	int server = gethostbyname(hostname); //http://beej.us/guide/bgnet/output/html/multipage/gethostbynameman.html
-	if (server == NULL)
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	
+	
+	
+	
+	int netserverinit(char * hostname)
 	{
-		fprintf(stderr,"HOST_NOT_FOUND");
-		return -1;
 		
 		
-	}
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct hostent *server;
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port   = htons(62135);  // Could be anything
+    inet_pton(AF_INET, hostname, &sin.sin_addr);
 	
-	int netopen(const char *pathname, int flags)
+	 serverIPAddress = gethostbyname(hostname);
+    if (serverIPAddress == NULL)
 	{
-		if(flags ==  O_RDONLY)
-	{
-		check = O_RDONLY;		
+        fprintf(stderr,"ERROR, no such host\n");
+        return -1;
 	}
-	else if( flags == O_WRONLY )
+    bzero((char *) &serverAddressInfo, sizeof(serverAddressInfo));
+	serverAddressInfo.sin_family = AF_INET;
+	serverAddressInfo.sin_port = htons(portno);
+	bcopy((char *)serverIPAddress->h_addr, (char *)&serverAddressInfo.sin_addr.s_addr, serverIPAddress->h_length);
+	
+	if (connect(sockfd,(struct sockaddr *)&serverAddressInfo,sizeof(serverAddressInfo)) < 0) 
 	{
-		check = O_WRONLY;
-	}
-	else if(flags ==  O_RDWR )
-	{
-		check =  O_RDWR;
+        error("success");
+		return 0;
 	}
 		
-		
 	}
 	
-	EACCES // permission denied
-	EINTR // interrupted function callo
 	
 	
 	
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Declare initial vars
+    							// Super-special secret C struct that holds info about a machine's address
+    
+	
+	
+	// If the user didn't enter enough arguments, complain and exit
+    if (argc < 3)
+	{
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+	
+	
+	
+	/** If the user gave enough arguments, try to use them to get a port number and address **/
+
+	// convert the text representation of the port number given by the user to an int
+	portno = atoi(argv[2]);
+	
+	// look up the IP address that matches up with the name given - the name given might
+	//    BE an IP address, which is fine, and store it in the 'serverIPAddress' struct
+    serverIPAddress = gethostbyname(argv[1]);
+    if (serverIPAddress == NULL)
+	{
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+				
+	// try to build a socket .. if it doesn't work, complain and exit
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+	{
+        error("ERROR creating socket");
 	}
+
+
 	
+	/** We now have the IP address and port to connect to on the server, we have to get    **/
+	/**   that information into C's special address struct for connecting sockets                     **/
+
+	// zero out the socket address info struct .. always initialize!
+    bzero((char *) &serverAddressInfo, sizeof(serverAddressInfo));
+
+	// set a flag to indicate the type of network address we'll be using 
+    serverAddressInfo.sin_family = AF_INET;
+	
+	// set the remote port .. translate from a 'normal' int to a super-special 'network-port-int'
+	serverAddressInfo.sin_port = htons(portno);
+
+	// do a raw copy of the bytes that represent the server's IP address in 
+	//   the 'serverIPAddress' struct into our serverIPAddressInfo struct
+    bcopy((char *)serverIPAddress->h_addr, (char *)&serverAddressInfo.sin_addr.s_addr, serverIPAddress->h_length);
 
 
 
+	/** We now have a blank socket and a fully parameterized address info struct .. time to connect **/
+	
+	// try to connect to the server using our blank socket and the address info struct 
+	//   if it doesn't work, complain and exit
+    if (connect(sockfd,(struct sockaddr *)&serverAddressInfo,sizeof(serverAddressInfo)) < 0) 
+	{
+        error("ERROR connecting");
+	}	
+	
+	
+	
+	/** If we're here, we're connected to the server .. w00t!  **/
+	
+    printf("Please enter the message: ");
+	
+	// zero out the message buffer
+    bzero(buffer,256);
+
+	// get a message from the client
+    fgets(buffer,255,stdin);
+    
+	// try to write it out to the server
+	n = write(sockfd,buffer,strlen(buffer));
+	
+	// if we couldn't write to the server for some reason, complain and exit
+    if (n < 0)
+	{
+         error("ERROR writing to socket");
+    }
+	
+	// sent message to the server, zero the buffer back out to read the server's response
+	bzero(buffer,256);
+
+	// read a message from the server into the buffer
+    n = read(sockfd,buffer,255);
+	
+	// if we couldn't read from the server for some reason, complain and exit
+    if (n < 0)
+	{
+         error("ERROR reading from socket");
+	}
+
+	// print out server's message
+    printf("%s\n",buffer);
 
 
-int netopen(const char *pathname, int flags) {
-	
-	int check  = 0;
-	
-	
-	
-	
-
-	
-	
-	
-	
+    return 0;
 }
